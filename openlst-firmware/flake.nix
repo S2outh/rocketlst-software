1400bb49fea7e86ddd9df09722602504040da945
eleocraft <eliseo.cailloux@gmx.de>
2026-01-08 19:27:58 +0100

    fixed nix setup and added firmware mux start script

--- openlst-firmware/flake.nix
+++ openlst-firmware/flake.nix
@@ -3,33 +3,29 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nixpkgs-python.url = "github:cachix/nixpkgs-python";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = {
 		nixpkgs,
-		nixpkgs-python,
 		flake-utils,
 		...
 	}: 
 	flake-utils.lib.eachDefaultSystem (
     system:
     let
-      pythonVersion = "3.7.0";
       pkgs = import nixpkgs { inherit system; };
-      buildPython = nixpkgs-python.packages.${system}.${pythonVersion};
     in
     {
       devShells.default = pkgs.mkShell {
-        buildInputs = [
-          buildPython
-					pkgs.gcc
-					pkgs.libgcc
+        buildInputs = with pkgs; [
+          python3
+					gcc
+					libgcc
 
-          pkgs.sdcc
-          pkgs.cc-tool
-          pkgs.picocom
+          sdcc
+          cc-tool
+          picocom
         ];
 				LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib";
         shellHook = ''
